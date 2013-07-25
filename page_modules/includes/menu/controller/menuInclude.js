cb34eb0e259dc61524273d42113cdea9d3beeff9
gregr <greg.weiqi@gmail.com>
2013-07-25 04:31:19 +0000

    fix menu stuff

--- page_modules/includes/menu/controller/menuInclude.js
+++ page_modules/includes/menu/controller/menuInclude.js
@@ -5,7 +5,7 @@ exports.page = function(acc) {
     pageObject.page.call(this, 'includes/menu', 'includes/menu');
     this.siteName=acc.global.siteName;
     this.pages = (acc.pages.map(function(page){
-        return '<li <% if ("'+page.displayName+'"==displayName) { %> class="active" <% } %> >  <a href="'+acc.domain+'/'+page.displayName+'">'+page.displayName+'<i class="icon-home"></i></a> </li>';
+        return '<li <% if ("'+page.displayName+'"==displayName) { %> class="active" <% } %> >  <a href="'+acc.domain+'/'+page.displayName+'">'+page.displayName+'<!--<i class="icon-home"></i>--></a> </li>';
     }).join(''));
     this.open = '<@';
     this.close = '@>';
