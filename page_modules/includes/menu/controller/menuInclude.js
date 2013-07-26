1047973c21013460d64ce7c98de74999a9103c9d
gregr <greg.weiqi@gmail.com>
2013-07-26 01:29:36 +0000

    fix menu stuff

--- page_modules/includes/menu/controller/menuInclude.js
+++ page_modules/includes/menu/controller/menuInclude.js
@@ -4,6 +4,7 @@ var pageObject = rek('pageObject.js');
 exports.page = function(acc) {
     pageObject.page.call(this, 'includes/menu', 'includes/menu');
     this.siteName=acc.global.siteName;
+    this.homePageUrl = acc.domain;
     this.pages = (acc.pages.map(function(page){
         return '<li <% if ("'+page.displayName+'"==displayName) { %> class="active" <% } %> >  <a href="'+acc.domain+'/'+page.displayName+'">'+page.displayName+'<!--<i class="icon-home"></i>--></a> </li>';
     }).join(''));
