528f98466a8548c8f366b5e3f612269c221fcd91
Trevor Baron <trevorjbaron@gmail.com>
2013-08-10 18:15:11 -0400

    add videos to blog

--- page_modules/blog/controller/blogPage.js
+++ page_modules/blog/controller/blogPage.js
@@ -16,14 +16,21 @@ exports.save = function(req, res, dbPage, callback) {
     if(req.body.deletePost){
         dbPage.blogPosts.splice(req.body.deletePost,1);
     }else if(req.body.submit){
-        dbPage.blogPosts.push(new exports.blogPost(req.body.postTitle, req.body.postText));
+        dbPage.blogPosts.push(new exports.blogPost(req.body.postTitle, req.body.postText, req.body.postVideo));
     }else{
         //dbPage.blogName=req.body.blogName;
     }
     callback("/editPage/"+dbPage.pageType+"/"+dbPage.displayName);
 }
 
-exports.blogPost = function(heading, text) {
+exports.blogPost = function(heading, text, video, picture) {
     this.heading=heading;
     this.text=text;
+    this.video='';
+    if(video){
+        var match = video.match(/v=(.*?)($| |&)/);
+        if(match.length >= 2){
+            this.video = '<iframe style="width:100%;max-width:560px;height:315px;" src="//www.youtube.com/embed/'+match[1]+'?rel=0" frameborder="0" allowfullscreen></iframe>';
+        }
+    }
 }
