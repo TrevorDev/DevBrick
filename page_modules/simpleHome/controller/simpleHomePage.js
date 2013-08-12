c6ec2fefa29b8b31ab2049b4645a672957397890
Trevor Baron <trevorjbaron@gmail.com>
2013-08-11 21:49:35 -0400

    fix img dimentions and add img upload to simplehome

--- page_modules/simpleHome/controller/simpleHomePage.js
+++ page_modules/simpleHome/controller/simpleHomePage.js
@@ -7,9 +7,19 @@ var fs = require('fs');
 exports.page = function() {
     pageObject.page.call(this, 'simpleHome', 'simpleHome');
     this.largeTitle = 'Simple Home Page';
+    this.miniTitle = 'hope you like it';
+    this.logo='';
 };
 
 exports.save = function(req, res, dbPage, callback) {
     dbPage.largeTitle=req.body.largeTitle;
-    callback();
+    dbPage.miniTitle=req.body.miniTitle;
+
+    pageObject.saveImg(req, req.files.logo, dbPage, 'logoPic'+(new Date()).toISOString().replace('.','-'), function(picFileLocation, err){
+    	console.log(picFileLocation);
+    	if(picFileLocation!==''){
+            dbPage.logo=picFileLocation;
+        }
+        callback(err);
+    });
 }
